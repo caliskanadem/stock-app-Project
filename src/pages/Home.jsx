7372de79e3addc4953d32340f3137b800bc26b21
caliskanadem <ad3m.calikan@gmail.com>
2023-07-11 22:03:04 +0100

    data

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,18 @@
+import { Typography } from "@mui/material";
 import React from "react";
+import KpiCards from "../components/KpiCards";
+import Charts from "../components/Charts";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Typography variant="h4" color="error">
+        DashBoard
+      </Typography>
+      <KpiCards />
+      <Charts />
+    </div>
+  );
 };
 
 export default Home;
